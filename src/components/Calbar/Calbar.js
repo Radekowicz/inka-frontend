88dcafaab6ff33a896a1f8612c47fe2912d115f0
Mateusz Łodziński <mateusz.lodzinski@7bulls.com>
2020-09-10 09:49:54 +0200

    scss

--- src/components/Calbar/Calbar.js
+++ src/components/Calbar/Calbar.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import "./Calbar.css";
+import "./Calbar.scss";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 
